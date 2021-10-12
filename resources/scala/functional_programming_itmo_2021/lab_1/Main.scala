a6bd835e63e874ab7c0221baab7fd2f276e769e4
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2021-10-12 03:39:26 +0300

    Added scala counterpart for lab_1

---
+++ resources/scala/functional_programming_itmo_2021/lab_1/Main.scala
@@ -0,0 +1,8 @@
+package scala.functional_programming_itmo_2021.lab_1
+
+import scala.functional_programming_itmo_2021.lab_1.Task10.task10Report
+import scala.functional_programming_itmo_2021.lab_1.Task21.task21Report
+
+object Main extends App:
+  println(task10Report[BigInt])
+  println(task21Report[Int])
