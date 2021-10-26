55654fae2e6b0fcfe93ca37316711e1d66cc30e0
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2021-10-26 18:13:56 +0300

    added proper main

--- src/functional_programming_itmo_2021/lab_2/main.clj
+++ src/functional_programming_itmo_2021/lab_2/main.clj
@@ -2,4 +2,18 @@
   (:require [functional-programming-itmo-2021.lab-2.hash-map :refer :all])
   )
 
-(defn -main [] (println "Lab2 Entry"))
+(defn -main []
+  (let [hashmap (open-address-map {9 9 10 10})
+        merged-with-example (merge hashmap example)
+        dissoced (dissoc merged-with-example 1 2 9)
+        updated (assoc dissoced 10 "OTHER VALUE")
+        retrieved-value (get updated 10)]
+      (do
+        (println "Example of working with Open-Addressing Hash Map")
+        (println "Example hash map: " example)
+        (println "Other hash map: " hashmap)
+        (println "Merged hash map: " merged-with-example)
+        (println "Dissoced hash map: " dissoced)
+        (println "Updated hash map: " updated)
+        (println "Retrieved value by key 10: " retrieved-value)
+        )))
