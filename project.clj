d4f0b91f5629c2a6ce34aa0a6bad604ae7f52c3f
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2021-11-09 06:49:16 +0300

    Updated project structure

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [nrepl/lein-nrepl "0.3.2"]]
+                 [nrepl/lein-nrepl "0.3.2"]
+                 [org.clojure/core.async "1.4.627"]]
   :profiles {
              :lab_1 {
                      :repl-options {
@@ -10,13 +11,6 @@
                                     :package functional-programming-itmo-2021.lab-1
                                     }
                      :main functional-programming-itmo-2021.lab-1.main
-                     }
-             :lab_2 {
-                     :repl-options {
-                                    :init-ns functional-programming-itmo-2021.lab-2.main
-                                    :package functional-programming-itmo-2021.lab-2
-                                    }
-                     :main functional-programming-itmo-2021.lab-2.main
                      }
              :lab_2 {
                      :repl-options {
